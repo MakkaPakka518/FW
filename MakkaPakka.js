cccd960b5d5fb33d366aa3effe9a36e4f8a6ce2e
玛卡巴卡 <ankang0518@gmail.com>
2026-09-15 22:19:01 +0800

    Add files via upload

---
+++ MakkaPakka.js
@@ -0,0 +1,115 @@
+{
+  "title": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖 𝙁𝙤𝙧 𝙍𝙚𝙭𝙣𝙤𝙬",
+  "description": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖‘𝙨 𝘾𝙝𝙖𝙣𝙣𝙚𝙡：t.me/MakkaPakkaOvO",
+  "icon": "https://raw.githubusercontent.com/MakkaPakka518/FW/refs/heads/main/widgets/tubiao/Rex-Makka.JPEG",
+  "widgets": [
+    {
+      "id": "guduo_hot_list_makka", 
+      "title": "骨朵热度指数榜", 
+      "description": "每日更新的CN剧集、动漫、综艺、电影全网热度排行", 
+      "requiredVersion": "0.0.1", 
+      "version": "1.0.1", 
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖", 
+      "url": "https://rex.fwd.ccwu.cc/js/gdrdbRex.js"
+    },
+    { 
+      "id": "super_ultime_media_hub_makka", 
+      "title": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖·终极聚合", 
+      "description": "动漫、影剧、综艺、流行风向与平台分流一网打尽", 
+      "requiredVersion": "0.0.1", 
+      "version": "1.3.0", 
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖", 
+      "url": "https://rex.fwd.ccwu.cc/js/mkbkRex.js" 
+    },
+    { 
+      "id": "makkapakka_hub_list_2.0", 
+      "title": "平台剧场", 
+      "description": "各個平臺劇場和豆瓣熱榜", 
+      "requiredVersion": "0.0.1", 
+      "version": "1.0.9", 
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖", 
+      "url": "https://rex.fwd.ccwu.cc/js/ptjcRex.js" 
+    },
+    { 
+      "id": "western_trends_hub_temp", 
+      "title": "欧美风向标|口碑与热度", 
+      "description": "聚合烂番茄(口碑)与流媒体平台(热度)", 
+      "requiredVersion": "0.0.1", 
+      "version": "1.1.5", 
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖", 
+      "url": "https://rex.fwd.ccwu.cc/js/westRex.js" 
+    },
+    { 
+      "id": "danmu_api_Max_binfa_Fast", 
+      "title": "极速弹幕", 
+      "description": "并发搜索多API、自定义名称、繁简互转、数量限制、关键词屏蔽、颜色重写（并发极速版，适合多api（api设置不同平台））", 
+      "requiredVersion": "0.0.2", 
+      "version": "1.3.4", 
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖", 
+      "url": "https://rex.fwd.ccwu.cc/js/jsdmRex.js" 
+    },
+    {
+      "id": "cinephile_hub_ultimate",
+      "title": "影迷宝藏 | 系列与流派",
+      "description": "聚合【系列电影大满贯】与【趣味流派探索】，一键补番，探索未知。",
+      "requiredVersion": "0.0.1",
+      "version": "1.0.5",
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖",
+      "url": "https://rex.fwd.ccwu.cc/js/ymbzRex.js"
+    },
+    {
+      "id": "variety_hub_ultimate_v4_fix",
+      "title": "全球综艺追更热度榜",
+      "description": "综艺更新时间表，热度榜",
+      "requiredVersion": "0.0.1",
+      "version": "2.0.4",
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖",
+      "url": "https://rex.fwd.ccwu.cc/js/qqzyRex.js"
+    },
+    {
+      "id": " global_series_makka_rex",
+      "title": "全球影视专区",
+      "description": "自由切换全球十几个国家与地区，探索纯正的本土电影与剧集",
+      "requiredVersion": "0.0.1",
+      "version": "2.1.1",
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖",
+      "url": "https://rex.fwd.ccwu.cc/js/qqyszq.js"
+    },
+    {
+      "id": "movie_ultimate_makka_v2_rex",
+      "title": "全能电影榜",
+      "description": "提供流行、高分、年度最佳以及按类型探索电影",
+      "requiredVersion": "0.0.1",
+      "version": "1.4.0",
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖",
+      "url": "https://rex.fwd.ccwu.cc/js/movie.js"
+    },
+    {
+      "id": "platform.originals.ui.fix",
+      "title": "流媒体·独家原创",
+      "description": "发现Netflix/HBO/腾讯/B站等平台自制内容",
+      "requiredVersion": "0.0.1",
+      "version": "1.0.6",
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖",
+      "url": "https://rex.fwd.ccwu.cc/js/lmtdjycRex.js"
+    },
+    {
+      "id": "anime_omni_fix_rex",
+      "title": "二次元全境聚合",
+      "description": "一站式聚合多平台动漫榜单。",
+      "requiredVersion": "0.0.1",
+      "version": "2.5.0",
+      "author": "𝙈𝙖𝙠𝙠𝙖𝙋𝙖𝙠𝙠𝙖",
+      "url": "https://rex.fwd.ccwu.cc/js/anime.js"
+    },
+    {
+      "id": "zhuijurili",
+      "title": "追剧日历",
+      "description": "追剧日历(今/明日播出、周历、各项榜单、今日推荐)",
+      "requiredVersion": "0.0.1",
+      "version": "1.0.4",
+      "author": "logvar",
+      "url": "https://rex.fwd.ccwu.cc/js/zjrllogvar.js"
+    }
+  ]
+}
